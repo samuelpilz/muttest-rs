907e47603db8ac72ade04e73e92ad0d9292dd009
Samuel Pilz <samuel.pilz@posteo.net>
2022-08-27 10:24:20 +0200

    add 'test' to ensure this lib does not break type inference downstream

--- api/lib.rs
+++ api/lib.rs
@@ -1,2 +1,11 @@
 pub use muttest_core::api::*;
 pub use muttest_codegen::mutate;
+
+// ensure that this is inferrable
+// see: https://github.com/llogiq/mutagen/issues/164
+// in particular, if you include `serde_json`, this will fail to compile
+#[cfg(test)]
+fn _ensure_this_is_inferrable() {
+    let x = [0u8];
+    assert_eq!((&x[..]).as_ref(), []);
+}
